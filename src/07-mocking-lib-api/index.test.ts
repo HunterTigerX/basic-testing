49e5de88b520efc7bd8e5fad068b3a39b70c69e3
HunterTigerX <HunterTigerX@gmail.com>
2024-02-12 16:37:46 +0300

    fix: fixed leaking in lib test, added extra check for test

--- src/07-mocking-lib-api/index.test.ts
+++ src/07-mocking-lib-api/index.test.ts
@@ -7,23 +7,33 @@ describe('throttledGetDataFromApi', () => {
   beforeEach(() => {
     axios.create = jest.fn(() => axios);
     axios.get = jest.fn().mockResolvedValue({ data: mockData });
+    jest.useFakeTimers();
+  });
+  afterEach(() => {
+    jest.useRealTimers();
+    jest.restoreAllMocks();
   });
 
   test('should create instance with provided base url', async () => {
     const axiosCreateSpy = jest.spyOn(axios, 'create');
     await throttledGetDataFromApi(relativePath);
+    jest.runAllTimers();
     expect(axiosCreateSpy).toHaveBeenCalledWith({
       baseURL: 'https://jsonplaceholder.typicode.com',
     });
   });
 
   test('should perform request to the correct provided url', async () => {
+    const axiosGetSpy = jest.spyOn(axios, 'get');
     const response = await throttledGetDataFromApi(relativePath);
+    jest.runAllTimers();
+    expect(axiosGetSpy).toHaveBeenCalledWith(relativePath);
     expect(response).toEqual(mockData);
   });
 
   test('should return response data', async () => {
     const response = await throttledGetDataFromApi(relativePath);
+    jest.runAllTimers();
     expect(response).toEqual(mockData);
   });
 });
